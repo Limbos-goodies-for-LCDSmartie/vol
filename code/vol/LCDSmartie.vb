f8690591750dbaee3654cef7c601e9fc73887289
limbo666 <ngeorgousis@hotmail.com>
2024-08-25 11:10:27 +0300

    Add files via upload

---
+++ code/vol/LCDSmartie.vb
@@ -0,0 +1,130 @@
+﻿Imports System.Text
+
+Public Class LCDSmartie
+
+
+    Dim pluginName As String = "vol"
+
+
+    Public Function function1(ByVal param1 As String, ByVal param2 As String) As String
+        If LCase(param1) = "about" Then
+            Return "returns volume level"
+
+        Else
+            Dim currentVolume As Integer = GetVolume()
+            Return currentVolume
+        End If
+
+    End Function
+
+
+    Public Function function2(ByVal param1 As String, ByVal param2 As String) As String
+        If LCase(param1) = "about" Then
+            Return "returns volume level in two digit"
+        Else
+            Dim currentVolume As Integer = GetVolume()
+
+            Return currentVolume.ToString("00")
+        End If
+
+
+    End Function
+
+    Public Function function3(ByVal param1 As String, ByVal param2 As String) As String
+        If LCase(param1) = "about" Then
+            Return "returns volume level in three digit"
+
+        Else
+            Dim currentVolume As Integer = GetVolume()
+
+            Return currentVolume.ToString("000")
+
+        End If
+
+    End Function
+
+
+    Public Function function4(ByVal param1 As String, ByVal param2 As String) As String
+
+
+        If LCase(param1) = "about" Then
+            Return "returns volume with percent symbol"
+        Else
+            Dim currentVolume As Integer = GetVolume()
+            Return currentVolume & "%"
+        End If
+
+
+    End Function
+
+
+
+
+
+    Public Function function20(ByVal param1 As String, ByVal param2 As String) As String
+        If LCase(param1) = "about" Then
+            Return "Created by Nikos Georgousis - 2024"
+
+        Else
+
+            Return pluginName & " v1.1 by Limbo"
+        End If
+
+
+    End Function
+
+
+
+    Public Function GetMinRefreshInterval() As Integer
+
+        Return 200 ' 200 ms (around 5 times a second)
+
+    End Function
+
+
+    Public Function SmartieDemo()
+
+
+
+        Dim demolist As New StringBuilder()
+        demolist.AppendLine("-- " & pluginName & " plugin for LCD Smartie")
+        demolist.AppendLine("returns system volume level")
+        demolist.AppendLine("This plugin can provide up to 5 values per second.")
+        demolist.AppendLine("------ Function1 ------")
+        demolist.AppendLine(">  returns volume level   <")
+        demolist.AppendLine("Current value  $dll(vol,1, , )")
+        demolist.AppendLine("------ Function2 ------")
+        demolist.AppendLine(">  returns volume level in two digit <")
+        demolist.AppendLine("Current value  $dll(vol,2, , )")
+        demolist.AppendLine("------ Function3 ------")
+        demolist.AppendLine(">   returns volume level in three digit   <")
+        demolist.AppendLine("Current value  $dll(vol,1, , )")
+        demolist.AppendLine("------ Function4 ------")
+        demolist.AppendLine(">  returns volume level followed by percent symbol   <")
+        demolist.AppendLine("Current value  $dll(vol,2, , )")
+        demolist.AppendLine("------ Function20 ------")
+        demolist.AppendLine(">  About <")
+        demolist.AppendLine("Get about info:  $dll(vol,20, , )")
+        demolist.AppendLine("")
+        demolist.AppendLine("** Visit **")
+        demolist.AppendLine("> Forums")
+        demolist.AppendLine("https://www.lcdsmartie.org")
+        demolist.AppendLine("> Active development branch (latest version)")
+        demolist.AppendLine("https://github.com/LCD-Smartie/LCDSmartie")
+        demolist.AppendLine("> My plugins can be found on:")
+        demolist.AppendLine("https://github.com/Limbos-goodies-for-LCDSmartie")
+        demolist.AppendLine("")
+        '  demolist.AppendLine("Note: This is a note for " & pluginName)
+
+
+        Dim result As String = demolist.ToString()
+        Return result
+    End Function
+
+    Public Function SmartieInfo()
+        Return "Developer: Nikos Georgousis (limbo)" & vbNewLine & "Version: 1.1 "
+    End Function
+
+
+
+End Class
---
+++ code/vol/Module1.vb
@@ -0,0 +1,82 @@
+﻿Option Strict Off
+Imports System
+Imports System.Runtime.InteropServices
+
+Module WindowsSystemAudio
+    <ComImport>
+    <Guid("A95664D2-9614-4F35-A746-DE8DB63617E6"),
+     InterfaceType(ComInterfaceType.InterfaceIsIUnknown)>
+    Private Interface IMMDeviceEnumerator
+#Disable Warning IDE1006 ' Naming Styles
+        Sub lpVtbl()
+#Enable Warning IDE1006 ' Naming Styles
+        Function GetDefaultAudioEndpoint(ByVal dataFlow As Integer,
+        ByVal role As Integer, <Out> ByRef ppDevice As IMMDevice) As Integer
+    End Interface
+
+    Private NotInheritable Class MMDeviceEnumeratorFactory
+        Public Shared Function CreateInstance() As IMMDeviceEnumerator
+            Return CType(Activator.CreateInstance(Type.GetTypeFromCLSID(New Guid("BCDE0395-E52F-467C-8E3D-C4579291692E"))), IMMDeviceEnumerator) ' a MMDeviceEnumerator
+        End Function
+    End Class
+
+    <Guid("D666063F-1587-4E43-81F1-B948E807363F"),
+     InterfaceType(ComInterfaceType.InterfaceIsIUnknown)>
+    Private Interface IMMDevice
+        Function Activate(
+    <MarshalAs(UnmanagedType.LPStruct)> ByVal iid As Guid,
+     ByVal dwClsCtx As Integer, ByVal pActivationParams As IntPtr, <Out>
+    <MarshalAs(UnmanagedType.IUnknown)> ByRef ppInterface As Object) As Integer
+    End Interface
+
+    <Guid("5CDF2C82-841E-4546-9722-0CF74078229A"),
+     InterfaceType(ComInterfaceType.InterfaceIsIUnknown)>
+    Public Interface IAudioEndpointVolume
+        Function RegisterControlChangeNotify(ByVal pNotify As IntPtr) As Integer
+        Function UnregisterControlChangeNotify(ByVal pNotify As IntPtr) As Integer
+        Function GetChannelCount(ByRef pnChannelCount As UInteger) As Integer
+        Function SetMasterVolumeLevel(ByVal fLevelDB As Single,
+        ByVal pguidEventContext As Guid) As Integer
+        Function SetMasterVolumeLevelScalar_(ByVal fLevel As Single, ByVal pguidEventContext As Guid) As Integer
+        Function GetMasterVolumeLevel(ByRef pfLevelDB As Single) As Integer
+        Function GetMasterVolumeLevelScalar(ByRef pfLevel As Single) As Integer
+    End Interface
+
+    Friend Sub SetVolume(ByVal Level As Integer)
+        Try
+            Dim deviceEnumerator As IMMDeviceEnumerator =
+                                 MMDeviceEnumeratorFactory.CreateInstance()
+            Dim speakers As IMMDevice = Nothing
+            Dim res As Integer
+            Const eRender = 0
+            Const eMultimedia = 1
+            deviceEnumerator.GetDefaultAudioEndpoint(eRender, eMultimedia, speakers)
+            Dim Audio_EndPointVolume As IAudioEndpointVolume = Nothing
+            speakers.Activate(GetType(IAudioEndpointVolume).GUID, 0, IntPtr.Zero, Audio_EndPointVolume)
+            Dim ZeroGuid As New Guid()
+            res = Audio_EndPointVolume.SetMasterVolumeLevelScalar_(Level / 100.0F, ZeroGuid)
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Friend Function GetVolume() As Integer
+        Try
+            Dim currentLevel As Single = 0 ' Expressed as a decimal value
+            Dim deviceEnumerator As IMMDeviceEnumerator =
+                                 MMDeviceEnumeratorFactory.CreateInstance()
+            Dim speakers As IMMDevice = Nothing
+            Dim res As Integer
+            Const eRender = 0
+            Const eMultimedia = 1
+            deviceEnumerator.GetDefaultAudioEndpoint(eRender, eMultimedia, speakers)
+            Dim Audio_EndPointVolume As IAudioEndpointVolume = Nothing
+            speakers.Activate(GetType(IAudioEndpointVolume).GUID,
+                              0, IntPtr.Zero, Audio_EndPointVolume)
+            res = Audio_EndPointVolume.GetMasterVolumeLevelScalar(currentLevel)
+            Return CInt(100 * currentLevel)  ' Returned as an Integer 0 - 100
+        Catch ex As Exception
+            Return -1
+        End Try
+    End Function
+End Module
+
